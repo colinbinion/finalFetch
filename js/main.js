dde2ef31db2454a3046e3fdcd2862753d3fcf1f5
colinbinion <colin.binion@me.com>
2016-12-15 12:02:46 -0700

    tested & completed data requests

--- js/main.js
+++ js/main.js
@@ -11,10 +11,25 @@ $(document).ready(function(){
 
 //  subaccount list
 
-	$("button").click(function(){
-        $.ajax({url: "https://api.vitelity.net/api.php?login=kgetrite&pass=C0l0rad0&cmd=subaccounts&do=list",
-			success: function(result){
-            	$("#div1").html(result);
+	    $("button").click(function(){
+        $.ajax({url: "https://cors-anywhere.herokuapp.com/https://api.vitelity.net/api.php?login=kgetrite&pass=C0l0rad0&cmd=subaccounts&do=list&xml=yes", success: function(result){
+			console.log(result)
+			var $result= $(result)
+			console.log($result)
+			var $subaccounts = $result.find("subaccount")
+			console.log($subaccounts)
+			$subaccounts.each(function(i,subaccount) {
+				
+				var $li = $('<li>')
+				var $subaccount = $(subaccount)
+				var subtxt =($subaccount.text())
+				$li.text(subtxt)
+				
+				var $sublist = $('#sublist')
+				$sublist.append($li)
+				console.log($subaccount.text())
+			
+			})
         }});
     });
 	
