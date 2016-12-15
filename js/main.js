0f63977f71d418daa4d961b7885bd9e9a5c047b7
colinbinion <colin.binion@me.com>
2016-12-15 00:16:48 -0700

    adding ajax

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+$(document).ready(function(){
+	
+//	balance
+	
+    $("button").click(function(){
+        $.ajax({url: "http://api.vitelity.net/api.php?login=kgetrite&pass=C0l0rad0&cmd=balance",
+			success: function(result){
+            	$("#div1").html(result);
+        }});
+    });
 
+//  subaccount list
+
+	$("button").click(function(){
+        $.ajax({url: "https://api.vitelity.net/api.php?login=kgetrite&pass=C0l0rad0&cmd=subaccounts&do=list",
+			success: function(result){
+            	$("#div1").html(result);
+        }});
+    });
+	
+//	    $("button").click(function(){
+//        $.ajax({url: "https://api.vitelity.net/api.php?login=kgetrite&pass=C0l0rad0&cmd=subaccounts&do=list", success: function(result){
+//            $("#div1").html(result);
+//        }});
+//    });
+	
+});
